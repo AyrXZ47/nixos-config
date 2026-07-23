665669b996fdd0a2ebc9d91e4cd5f5bf6caf303c
false <false>
2026-07-22 22:51:55 -0600

    refactor: remove waybar mkForce to restore HyDE dynamic styling

--- modules/hm/default.nix
+++ modules/hm/default.nix
@@ -2,38 +2,16 @@
 
 {
   imports = [ ];
-
   home.packages = [ ];
-
   hydenix.hm.enable = true;
-
-# --- [ WAYBAR A LA DERECHA CON ESTÉTICA INTACTA ] ---
-  home.file.".config/waybar/config" = lib.mkForce {
-    text = ''
-      {
-        "include": [
-          "~/.config/waybar/modules.json",
-          "~/.config/waybar/modules/*.json",
-          "~/.config/waybar/modules/*.jsonc"
-        ],
-        "layer": "top",
-        "position": "right",
-        "width": 44,
-        "modules-left": ["hyprland/workspaces"],
-        "modules-center": ["clock"],
-        "modules-right": ["tray", "custom/power"]
-      }
-    '';
-  };
 
   # --- [ HYPRLAND PREFS: RESOLUCIÓN Y TRANSPARENCIA ] ---
   home.file.".config/hypr/userprefs.conf" = lib.mkForce {
     text = ''
-      # Forzar resolución desde el segundo cero (soporte específico y genérico)
       monitor = Virtual-1, 1920x1080@60, auto, 1
       monitor = , 1920x1080@60, auto, 1
 
-      # Forzamos la transparencia y quitamos el blur feo de Waybar
+      # Quitar el blur de Waybar sin romper su CSS
       layerrule = ignorealpha 1, waybar
       layerrule = noanim, waybar
     '';
