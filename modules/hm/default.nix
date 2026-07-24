d02b85d51139caf48b165408fb2d9e3efae41b3a
false <false>
2026-07-23 19:17:02 -0600

    style: enforce symmetrical gaps across waybar and hyprland 7

--- modules/hm/default.nix
+++ modules/hm/default.nix
@@ -37,7 +37,7 @@
       # Gaps simétricos y quirúrgicos para alinear ventanas con Waybar (3px)
       general {
           gaps_in = 6
-          gaps_out = 11
+          gaps_out = 14
       }
 
       # Blindar animaciones para siempre
