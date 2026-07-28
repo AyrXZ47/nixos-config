89a338a2b469a7ad1fbb161164232f26f396cc0c
Yovick RZ <66042604+AyrXZ47@users.noreply.github.com>
2026-07-28 00:56:56 -0600

    feat(boot): silent luks plymouth prompt via initrd.verbose=false, optimize for virgl 3d

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -22,6 +22,7 @@ in
 
     environment.sessionVariables = {
       NIXOS_OZONE_WL = "1";
+      WLR_NO_HARDWARE_CURSORS = "1";
     };
 
     environment.systemPackages = with pkgs; [
--- modules/theming/plymouth.nix
+++ modules/theming/plymouth.nix
@@ -8,6 +8,7 @@
   };
 
   boot.initrd.systemd.enable = true;
+  boot.initrd.verbose = false;
   boot.consoleLogLevel = 0;
   boot.kernelParams = [
     "quiet"
