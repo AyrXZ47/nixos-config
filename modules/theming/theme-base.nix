e51910132d5671bbb13f40f00fce3fea9d8ebea5
Yovick R. Z. <yovickrz@gmail.com>
2026-08-19 21:40:07 -0600

    chore(theming): quitar override de cursor Bibata -> default

--- modules/theming/theme-base.nix
+++ modules/theming/theme-base.nix
@@ -1,15 +1,6 @@
 { config, pkgs, lib, ... }:
 
 {
-  home-manager.users.yovick = {
-    home.pointerCursor = {
-      enable = true;
-      name = "Bibata-Modern-Classic";
-      package = pkgs.bibata-cursors;
-      size = 24;
-    };
-  };
-
   fonts.packages = with pkgs; [
     jetbrains-mono
     nerd-fonts.jetbrains-mono
